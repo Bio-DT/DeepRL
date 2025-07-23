56c9a005f0d9b4ab6ca8bbd658a70e3baee0c274
Bio-DT <3171478932@qq.com>
2025-07-23 16:04:30 +0800

    Update train.py

--- code/DTA/train.py
+++ code/DTA/train.py
@@ -9,12 +9,6 @@ from models.deeprlginconv import DeepRL_DTA_model_GIN
 from utils import *
 
 
-'''
-python /media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/train.py 0 0 0  ---第一个参数:数据-davis   第二个参数:选模型   第三个参数:GPU
-python /media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/train.py 1 1 0  ---第一个参数:数据-kiba   第二个参数:选模型   第三个参数:GPU
-
-'''
-
 
 # training function at each epoch
 def train(model, device, train_loader, optimizer, epoch):
@@ -62,7 +56,7 @@ if len(sys.argv)>3:
     cuda_name = "cuda:" + str(int(sys.argv[3])) 
 print('cuda_name:', cuda_name)
 
-# # 选择 GPU 或 CPU 作为设备
+#
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -72,8 +66,8 @@ LR = 0.00005  #0.0005
 LOG_INTERVAL = 20
 NUM_EPOCHS = 1000
 
-save_model = '/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/save_model'
-save_result = '/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/save_result'
+save_model = './DeepRL/DTA/save_model'
+save_result = './DeepRL/DTA/save_result'
 
 print('Learning rate: ', LR)
 print('Epochs: ', NUM_EPOCHS)
@@ -81,13 +75,13 @@ print('Epochs: ', NUM_EPOCHS)
 # Main program: iterate over different datasets
 for dataset in datasets:
     print('\nrunning on ', model_st + '_' + dataset )
-    processed_data_file_train = '/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/data/processed/' + dataset + '_train.pt'
-    processed_data_file_test = '/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/data/processed/' + dataset + '_test.pt'
+    processed_data_file_train = './DeepRL/DTA/data/processed/' + dataset + '_train.pt'
+    processed_data_file_test = './DeepRL/DTA/data/processed/' + dataset + '_test.pt'
     if ((not os.path.isfile(processed_data_file_train)) or (not os.path.isfile(processed_data_file_test))):
         print('please run create_data.py to prepare data in pytorch format!')
     else:
-        train_data = TestbedDataset(root='/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/data', dataset=dataset+'_train')
-        test_data = TestbedDataset(root='/media/estar/98519505-51e9-4e2d-b09d-5a389290bcd9/yh/DeepRL/DTA/data', dataset=dataset+'_test')
+        train_data = TestbedDataset(root='./DeepRL/DTA/data', dataset=dataset+'_train')
+        test_data = TestbedDataset(root='./DeepRL/DTA/data', dataset=dataset+'_test')
 
         
         # make data PyTorch mini-batch processing ready
@@ -102,20 +96,16 @@ for dataset in datasets:
         # load_model_path = os.path.join(save_model, 'model_' + model_st + '_' + dataset +  '.pt')
         # if os.path.exists(load_model_path):
         #     save_file_dict = torch.load(load_model_path)
-        #     # 处理参数名称（如果保存时使用了多GPU训练，参数名称可能带有 "module." 前缀）
         #     new_save_file_dict = {k.replace("module.", ""): v for k, v in save_file_dict.items()}
             
-        #     # 获取当前模型的 state_dict
         #     model_state_dict = model.state_dict()
-            
-        #     # 只加载匹配的参数
+        
         #     for key, value in new_save_file_dict.items():
         #         if key in model_state_dict and model_state_dict[key].shape == value.shape:
         #             model_state_dict[key] = value
         #         else:
         #             print(f"Skipping {key} due to size mismatch or missing key.")
             
-        #     # 加载调整后的参数
         #     model.load_state_dict(model_state_dict, strict=False)
         #     print(f"Model parameters loaded from {load_model_path}")
         # else:
@@ -143,7 +133,7 @@ for dataset in datasets:
 
 
             if ret[-1]>best_ci:  ##ret[1]<best_mse:  ret[-1]>best_ci
-                torch.save(model.state_dict(), name)   ###保存模型
+                torch.save(model.state_dict(), name)   ###save model
                 with open(result,'w') as f:
                     f.write(','.join(map(str,ret)))
                 best_epoch = epoch+1
