d603a259126c33112e64e9aef6cacc4b05954da4
AHAU-Bio <3171478932@qq.com>
2025-07-23 14:55:52 +0800

    Add files via upload

---
+++ code/DTA/models/deeprlgatgcn.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Sequential, Linear, ReLU
+from torch_geometric.nn import GCNConv, GATConv, GINConv, global_add_pool
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+
+from DeepRL_DTA import *
+
+
+class DeepRL_DTA_model_GAT(torch.nn.Module):
+    def __init__(self, n_output=1, num_features_xd=78, num_features_xt=25,
+                 n_filters=32, embed_dim=128, output_dim=128, dropout=0.2):
+
+        super(DeepRL_DTA_model_GAT, self).__init__()
+
+        self.n_output = n_output
+        self.conv1 = GATConv(num_features_xd, num_features_xd, heads=10)
+        self.conv2 = GCNConv(num_features_xd*10, num_features_xd*10)
+        self.fc_g1 = torch.nn.Linear(num_features_xd*10*2, 1500)
+        self.fc_g2 = torch.nn.Linear(1500, output_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+
+        # 1D convolution on protein sequence
+        self.embedding_xt = nn.Embedding(num_features_xt + 1, embed_dim)
+        self.conv_xt_1 = nn.Conv1d(in_channels=1000, out_channels=n_filters, kernel_size=8)
+        self.fc1_xt = nn.Linear(32*121, output_dim)
+
+        # combined layers
+        self.fc1 = nn.Linear(256, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.out = nn.Linear(512, self.n_output)        # n_output = 1 for regression task
+
+        self.molecule_encoder = Transformer_lar()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        target = data.target
+        smile = data.smile
+        # print('x shape = ', x.shape)
+        x = self.conv1(x, edge_index)
+        x = self.relu(x)
+        x = self.conv2(x, edge_index)
+        x = self.relu(x)
+        # apply global max pooling (gmp) and global mean pooling (gap)
+        x = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+        x = self.relu(self.fc_g1(x))
+        x = self.dropout(x)
+        x_sub = self.fc_g2(x)
+
+        ####protein sequence embedding
+        embedded_xt_sub = self.embedding_xt(target)
+        embedded_xt_llm = self.molecule_encoder(target)  #### embedding protein sequence by llm
+        beta = 0.5  ##0.3
+        embedded_xt = beta*embedded_xt_sub + (1-beta)*embedded_xt_llm
+        conv_xt = self.conv_xt_1(embedded_xt) 
+        # flatten
+        xt = conv_xt.view(-1, 32 * 121)
+        xt = self.fc1_xt(xt)
+
+        ###drug smiles embedding
+        # embedded_xd_sub = self.embedding_xt(smile)  ###embedding drug smiles 
+        embedded_xd_llm = self.molecule_encoder(smile)  #### embedding drug smiles by llm       
+        # beta = 0.5
+        # embedded_xd = beta*embedded_xd_sub + (1-beta)*embedded_xd_llm
+        conv_xd = self.conv_xt_1(embedded_xd_llm)
+        # # flatten
+        xd = conv_xd.view(-1, 32 * 121)
+        xd = self.fc1_xt(xd)
+        alpha = 0.5 ##0.3-->ci=0.897568 mes=0.22174
+        x = alpha*x_sub + (1-alpha)*xd  ###drug_graph+drug_smiles
+        # print("========1111=========",conv_xd.shape)
+        # print("========2222=========",xd.shape)
+        # print("========3333=========",xt.shape)
+        # exit()
+
+        # concat
+        xc = torch.cat((x, xt), 1)
+        # add some dense layers
+        xc = self.fc1(xc)
+        xc = self.relu(xc)
+        xc = self.dropout(xc)
+        xc = self.fc2(xc)
+        xc = self.relu(xc)
+        xc = self.dropout(xc)
+        out = self.out(xc)
+        return out
