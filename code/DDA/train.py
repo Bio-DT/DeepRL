b4884fe937c2db29727c95db1fc9e68445ba680d
Bio-DT <3171478932@qq.com>
2025-07-23 16:20:58 +0800

    Update train.py

--- code/DDA/train.py
+++ code/DDA/train.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as fn
 from preprocessing import *
-from model.DeepRL_DDA import DeepRL_DDA_model
+from model.DeepRL_DDA_model import DeepRL_DDA_model
 from metric import *
 
 
